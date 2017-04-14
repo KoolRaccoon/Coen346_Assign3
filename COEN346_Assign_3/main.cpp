f8286d4b6f7f6c4598351f11252c26968731ab62
Felix <felixadrianlucaciu@gmail.com>
2017-04-14 11:19:17 -0400

    Fixed parameter in function 'takeProcess'

--- COEN346_Assign_3/main.cpp
+++ COEN346_Assign_3/main.cpp
@@ -7,10 +7,34 @@
 
 using namespace std;
 
-//void takeProcess(vector<Process*> &ProcessQ, Clock clk); //Trying to pass in Clock object. Doesn't work...
-/* Maybe you shouldn't pass Clock clk, but just "clk" instead since you're not defining the function, only declaring it*/
-void takeProcess(vector<Process*> &ProcessQ);
+void takeProcess(vector<Process*> &ProcessQ, Clock& clk); //Trying to pass in Clock object. Doesn't work...
+//void takeProcess(vector<Process*> &ProcessQ);
 Process* tempProc;
+
+
+void takeProcess(vector<Process*> &ProcessQ, Clock& clk){
+    int counter = 1;
+    while(ProcessQ.size()>0){
+        cout << "Function ran: " << counter << " times" << endl;
+        tempProc = ProcessQ.front();
+        
+        cout << tempProc->getbT()<<endl;
+        
+        cout << "Vector ProcessQ is of size: " << ProcessQ.size() << endl;
+        for(int i = 0; i<ProcessQ.size(); i++){
+            if(i<ProcessQ.size()-1){
+                ProcessQ.at(i) = ProcessQ.at(i+1);
+            }else{
+                ProcessQ.at(i)= NULL;
+            }
+        }
+        
+        ProcessQ.pop_back();
+        
+        cout << "Vector ProcessQ is of size: " << ProcessQ.size() << endl;
+        counter++;
+    }
+}
 
 
 // Right now, I initiated the clock object. However, when I try to pass it to the function, it crashes. Idk what the issue is.
@@ -19,7 +43,7 @@ int main()
 {
     Clock clk;
     clk.StartClock();
-    cout<< "Time: " << clk.getTime() <<endl;
+    cout<< "Time: " << clk.getTime() << endl;
     Process *pt1;
     Process *pt2;
     Process p1(1000,2500);
@@ -34,10 +58,12 @@ int main()
     ProcessQ.push_back(&p3);
 
 //    std::thread mythread(&takeProcess, std::ref(ProcessQ), std::ref(clk)); // Attempting to passe Clock object to thread
-    std::thread mythread(&takeProcess, std::ref(ProcessQ));
+    std::thread mythread(&takeProcess, std::ref(ProcessQ), std::ref(clk));
+    
     for (int i = 0; i<10; i++){
-        cout <<"Printing from main thread"<<endl;
+        cout << "Printing from main thread" << endl;
     }
+    
     mythread.join();
 
 /*    ProcessList[0] = p1;
@@ -48,28 +74,4 @@ int main()
     cout << "Process pointer 1 is pointing at index 0 and its process' arrival time is " << pt1->getaT() << endl;
     cout << "Process pointer 2 is pointing at index 1, and its process' arrival time is " << pt2->getaT() <<endl;
 */    return 0;
-}
-
-void takeProcess(vector<Process*> &ProcessQ/*, Clock clk*/){
-    int counter = 1;
-    while(ProcessQ.size()>0){
-            cout << "Function ran: " << counter << " times" << endl;
-    tempProc = ProcessQ.front();
-
-    cout << tempProc->getbT()<<endl;
-
-    cout << "Vector ProcessQ is of size: " << ProcessQ.size() << endl;
-    for(int i = 0; i<ProcessQ.size(); i++){
-        if(i<ProcessQ.size()-1){
-        ProcessQ.at(i) = ProcessQ.at(i+1);
-        }else{
-        ProcessQ.at(i)= NULL;
-        }
-    }
-
-    ProcessQ.pop_back();
-
-    cout << "Vector ProcessQ is of size: " << ProcessQ.size() << endl;
-    counter++;
-    }
 }
